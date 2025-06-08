f46161fd0aa7e2e61134ab8c8ee850da7be57332
Puripat13 <pph656512@gmail.com>
2025-06-08 16:45:12 +0700

    Update upload_to_gdrive.py

--- upload_to_gdrive.py
+++ upload_to_gdrive.py
@@ -1,11 +1,14 @@
-
 import os
+import json
 from pydrive2.auth import ServiceAccountCredentials
 from pydrive2.drive import GoogleDrive
 
 def upload_file_to_drive(filepath, folder_id):
+    creds_json = os.environ.get("GOOGLE_CREDENTIALS_JSON")
+    creds_dict = json.loads(creds_json)
+
     scopes = ["https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scopes)
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
     drive = GoogleDrive(creds)
 
     file_name = os.path.basename(filepath)
@@ -13,3 +16,4 @@ def upload_file_to_drive(filepath, folder_id):
     file_drive.SetContentFile(filepath)
     file_drive.Upload()
     print(f"✅ อัปโหลดไฟล์ {file_name} ไปยัง Google Drive สำเร็จ")
+
